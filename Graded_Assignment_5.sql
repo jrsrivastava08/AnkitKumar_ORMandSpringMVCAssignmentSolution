3763d5c3c87ecf80d1f76cf17eadde8dd60823d3
Ankit Kumar <kummarankit1997@gmail.com>
2022-03-20 21:54:35 +0530

    DBMS

---
+++ Graded_Assignment_5.sql
@@ -0,0 +1,5 @@
+use `customersdb`;
+
+insert into customers(First_Name, Last_Name, Email) values("Harshit", "Choudhary", "Harshit@greatlearning.in");
+insert into customers(First_Name, Last_Name, Email) values("Harshit", "Choudhary", "Harshit@greatlearning.in");
+select * from customers;
